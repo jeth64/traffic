96eef3d99ba400b135aa1af772374d042f4baf19
jeth64 <j.massa@mail.de>
2014-06-06 22:37:03 +0200

    initiate om-templating

--- project.clj
+++ project.clj
@@ -5,19 +5,26 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [kioo "0.4.0"]
+                 [om "0.5.1"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
 
-  :source-paths ["src/clj" "src/cljs"]
+  :source-paths ["src/clj"]
+  :resource-paths ["resources"]
 
-  :hooks [leiningen.cljsbuild]
-
-  :cljsbuild {:builds {:dev {:source-paths ["src/cljs"]
-                             :compiler {:output-to "assets/www/js/index.js"
-                                        :optimizations :simple
-                                        :pretty-print true}}
-                       :prod {:source-paths ["src/cljs"]
-                              :compiler {:output-to "assets/www/js/index.js"
-                                         :optimizations :advanced
-                                         :pretty-print false}}}})
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src/cljs"]
+                        :compiler {:output-to "assets/www/js/index.js"
+                                   :optimizations :simple
+                                   :pretty-print true
+                                   :preamble ["react/react.js"]
+                                   :externs ["react/externs/react.js"]}}
+                       {:id "prod"
+                        :source-paths ["src/cljs"]
+                        :compiler {:output-to "assets/www/js/index.js"
+                                   :optimizations :advanced
+                                   :pretty-print false
+                                   :preamble ["react/react.js"]
+                                   :externs ["react/externs/react.js"]}}]})
--- src/cljs/traffic/app.cljs
+++ src/cljs/traffic/app.cljs
@@ -25,3 +25,37 @@
 (defn  ^:export initialize []
   (fill-store "traffic-signs" )
   (.addEventListener js/document "deviceready" onDeviceReady true))
+
+;;
+;; page setup
+;;
+
+(defsnippet my-nav-item "main.html" [:.nav-item]
+  [[caption func]]
+  {[:a] (do-> (content caption)
+              (listen :onClick #(func caption)))})
+
+(defsnippet my-header "main.html" [:header]
+  [{:keys [heading]}]
+  {[:h1] (content heading)})
+
+(defsnippet my-navigation "main.html" [:.content]
+  [{:keys [navigation]}]
+  {[:table] (content (map my-nav-item navigation))})
+
+
+(deftemplate my-page "main.html"
+  [data]
+  {[:header] (substitute (my-header data))
+   [:.content] (substitute (my-navigation data))})
+
+(defn init [data]
+  (initialize)
+  (om/component (my-page data)))
+
+(def app-state (atom {:heading "main"
+                      :content "Hello World"
+                      :navigation [["home" #(js/alert %)]
+                                   ["next" #(js/alert %)]]}))
+
+(om/root init app-state {:target  (.-body js/document)})
