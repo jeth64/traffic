e38de02800f42c48f396836c8188ddcb2223979b
jeth64 <j.massa@mail.de>
2014-06-21 03:17:10 +0200

    add detail page

--- src/cljs/traffic/app.cljs
+++ src/cljs/traffic/app.cljs
@@ -1,11 +1,15 @@
 (ns traffic.app
-  (:require [traffic.storage :refer [fill-store]]
-            [kioo.om :refer [content set-attr do-> substitute listen]]
+  (:require [traffic.storage :refer [fill-store empty-store get-stored]]
+            [kioo.om :refer [content set-attr do-> substitute listen wrap]]
             [kioo.core :refer [handle-wrapper]]
             [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true])
+            [om.dom :as dom :include-macros true]
+            [cljs.reader :refer [read-string]])
   (:require-macros [kioo.om :refer [defsnippet deftemplate]]))
 
+(declare main-page main-state)
+
+(def signmapname "traffic-signs")
 
 ;;
 ;; Exported functions
@@ -19,46 +23,88 @@
 
 
 (defn ^:export onDeviceReady []
-  (prompt "PhoneGap is working"))
+  (prompt "PhoneGap is working"
+          ))
 
 
 (defn  ^:export initialize []
-  (fill-store "traffic-signs" )
+  (empty-store)
+  (fill-store signmapname)
   (.addEventListener js/document "deviceready" onDeviceReady true))
 
 ;;
-;; page setup
+;; shortcuts for kioo templating
 ;;
 
-(defsnippet my-nav-item "main.html" [:#nav-item]
-  [[caption func]]
-  {[:a] (do-> (content caption)
-              (listen :onClick #(func caption)))})
 
-(defsnippet my-header "main.html" [:header]
+(defn jump-to [page state]
+  (om/root #(om/component (page %)) state {:target (.-body js/document)}))
+
+
+;;
+;; signs page setup
+;;
+
+(defsnippet detail-item "detail.html" [:#sign-entry]
+  [{:keys [url bedeutung]}]
+  {[:#sign-img] (set-attr :src (str "zeichen/" url))
+   [:#sign-meaning] (content bedeutung)})
+
+(defsnippet detail-header "detail.html" [:#navigation]
+  [{:keys [heading]}]
+  {[:#back] (listen :onClick #(jump-to main-page main-state))
+   [:#section-title] (content (str "    " heading))})
+
+(defsnippet detail-list "detail.html" [:#content]
+  [{:keys [list]}]
+  {[:#sign-list] (content (map detail-item list))})
+
+(deftemplate detail-page "detail.html"
+  [data]
+  {[:#navigation] (substitute (detail-header data))
+   [:#content] (substitute (detail-list data))})
+
+(defn detail-state [section]
+  (atom {:heading section
+         :list (filter #(= section (:kategorie %))
+                       (read-string (get-stored signmapname)))}))
+
+
+
+;;
+;; main page setup
+;;
+
+(defsnippet main-nav-item "main.html" [:#nav-item]
+  [caption]
+  {[:a] (do-> (content caption)
+              (listen :onClick #(jump-to detail-page (detail-state caption))))})
+
+(defsnippet main-header "main.html" [:header]
   [{:keys [heading]}]
   {[:h1] (content heading)})
 
-(defsnippet my-navigation "main.html" [:.content]
+(defsnippet main-navigation "main.html" [:.content]
   [{:keys [navigation]}]
-  {[:#navigation] (content (map my-nav-item navigation))})
+  {[:#navigation] (content (map main-nav-item navigation))})
 
-(deftemplate my-page "main.html"
+
+(deftemplate main-page "main.html"
   [data]
-  {[:header] (substitute (my-header data))
-   [:.content] (substitute (my-navigation data))})
+  {[:header] (substitute (main-header data))
+   [:.content] (substitute (main-navigation data))})
 
 
-(defn init [data]
- ;; (initialize)
-  (om/component (my-page data)))
+(def main-state (atom {:heading "Verkehrszeichen"
+                       :navigation ["Gefahrzeichen"
+                                   "Vorschriftzeichen"
+                                   "Richtzeichen"
+                                   "Verkehrslenkungstafeln"
+                                   "Verkehrseinrichtungen"
+                                   "Zusatzzeichen"
+                                   "Sinnbilder"
+                                   "Autobahnschilder"]}))
 
-(def app-state (atom {:heading "Verkehrszeichen"
-                      :navigation [["Gefahrzeichen" #(js/alert %)]
-                                   ["Vorschriftzeichen" #(js/alert %)]
-                                   ["Richtzeichen" #(js/alert %)]
-                                   ["Verkehrslenkungseinrichtung" #(js/alert %)]
-                                   ["Zusatzzeichen" #(js/alert %)]
-                                   ["Sinnbilder" #(js/alert %)]]}))
 
-(om/root init app-state {:target  (.-body js/document)})
+(initialize)
+(jump-to main-page main-state)
