e666a00f195047320d9cb2374ccee3b4b6e7deca
jeth64 <j.massa@mail.de>
2014-06-21 03:14:46 +0200

    correct regular expressions and encoding

--- src/cljs/traffic/storage.cljs
+++ src/cljs/traffic/storage.cljs
@@ -1,10 +1,16 @@
 (ns traffic.storage
+  (:require [clojure.string :refer [trim]])
   (:require-macros [traffic.macros :as tm]))
 
 
 ;;
 ;; local storage
 ;;
+
+(defn decode-html-entity [str]
+  (let [div (.createElement js/document "div")]
+    (aset div "innerHTML" str)
+    (.-nodeValue (.-firstChild div))))
 
 
 ;; general
@@ -25,11 +31,30 @@
   (.clear (.-localStorage js/window)))
 
 
+
 ;; specific
+
+(defn contains-re? [re target-str]
+  (.test re target-str))
+
+(defn create-record [[title meaning]]
+  {:url (.replace (str title ".png") (js/RegExp. (.-source #" ") "g") "_")
+   :bedeutung (decode-html-entity (trim meaning))
+   :kategorie (cond (contains-re? #"Zusatzzeichen" title) "Zusatzzeichen"
+                    (contains-re? #"Sinnbild" title) "Sinnbilder"
+                    (contains-re? #"Zeichen( |_)1[0-9][0-9]([^0-9]|$)" title) "Gefahrzeichen"
+                    (contains-re? #"Zeichen( |_)2[0-9][0-9]([^0-9]|$)" title) "Vorschriftzeichen"
+                    (contains-re? #"(Z|z)eichen( |_)[3-4][0-9][0-9]([^0-9]|$)" title) "Richtzeichen"
+                    (contains-re? #"Bundesautobahn 48" title) "Richtzeichen"
+                    (contains-re? #"Zeichen( |_)5[0-9][0-9]([^0-9]|$)" title) "Verkehrslenkungstafeln"
+                    (contains-re? #"Zeichen( |_)6[0-9][0-9]([^0-9]|$)" title) "Verkehrseinrichtungen"
+                    (contains-re? #"RWB" title) "Autobahnschilder"
+                    :else "-"
+                    )})
 
 (defn fill-store
   "Parse and store traffic signs as list of maps"
   [target-key]
   (if (= nil (get-stored target-key))
-    (store target-key (map #(zipmap [:title :bedeutung] %)
-                           (tm/sign-list "resources/zeichenmap")))))
+    (store target-key
+           (map create-record (tm/sign-list "resources/zeichenmap")))))
